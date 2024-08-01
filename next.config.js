cb7a9c19bd3e894d0b2daea43a407c6570c9494a
Art <art@lighttwist.io>
2024-07-31 20:05:00 -0400

    Clean up next.config.js and successful build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-import path from 'path';
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
